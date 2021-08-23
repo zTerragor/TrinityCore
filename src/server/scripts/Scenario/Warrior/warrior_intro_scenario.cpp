63ea93fb25f46afeca34fe9d5f5504501f20aba3
TheGhost <theghostgroup@gmail.com>
2021-08-23 18:49:47 +1200

    Core/Script: Add base script for warrior hall intro scenario.

---
+++ src/server/scripts/Scenario/Warrior/warrior_intro_scenario.cpp
@@ -0,0 +1,52 @@
+/*
+ * This file is part of the TrinityCore Project. See AUTHORS file for Copyright information
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* Warrior Hall: Intro Scenario */
+
+#include "ScriptMgr.h"
+#include "ScriptedCreature.h"
+#include "CreatureTextMgr.h"
+#include "GameObjectAI.h"
+#include "Object.h"
+
+
+enum npcs
+{
+    NPC_ASPIRING_VRYKUL_HERO = 102990,
+    NPC_FINNA_BJORNSDOTTIR = 97166,
+    NPC_RAGNVALD_DRAKEBORN = 97167,
+    NPC_EREDAR_ENSLAVER = 97176,
+    NPC_FEL_LORD_DAKUUR = 102970,
+    NPC_HAARDGRIM = 102964,
+    NPC_ALRIK_THE_SLAYER = 102965,
+    NPC_BEZZEREDES = 98257
+};
+
+enum spells
+{
+    SPELL_WHIRLWIND = 13736,
+    SPELL_MORTAL_STRIKE = 15708,
+    SPELL_THUNDERCLAP = 15588,
+    SPELL_CLEAVE = 16044
+};
+
+
+
+void AddSC_warrior_intro_scenario()
+{
+
+}
