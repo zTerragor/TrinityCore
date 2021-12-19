45cbe2903820f1228ea74aa8b6f817532a198cdf
Nyr <nyrdeveloper@gmail.com>
2021-12-19 20:03:07 +0100

    Raid/SOD: removing unnecesary attribute from Barbed Shot.
    * added enums for Texts.
    * added missing space on header.
    * removed unnecesary additions on script_loader.cpp.

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -4616,8 +4616,6 @@ void SpellMgr::LoadSpellInfoCorrections()
     // Barbed Shot
     ApplySpellFix({ 347807 }, [](SpellInfo* spellInfo)
     {
-        spellInfo->AttributesEx3 &= ~SPELL_ATTR3_STACK_FOR_DIFF_CASTERS;
-
         spellInfo->AuraInterruptFlags |= SpellAuraInterruptFlags::LeavingCombat;
         spellInfo->AuraInterruptFlags2 |= SpellAuraInterruptFlags2::EndOfEncounter;
     });
